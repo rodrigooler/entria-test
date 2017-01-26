8d215b8f2c216bd4cf5c2df8fb843a81316f9f89
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2017-01-26 00:48:31 -0200

    Reapproving the code

---
+++ app/MyApp.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import UserListView from './components/UserListView';
+
+export default class MyApp extends Component {
+  render() {
+    return (
+      <UserListView />
+    );
+  }
+};
---
+++ app/components/UserListView.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ListView
+} from 'react-native';
+
+export default class UserListView extends Component {
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(['Pikachu', 'Raichu']),
+    };
+  }
+
+  render() {
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) => <Text>{rowData}</Text>}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
