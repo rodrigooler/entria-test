6beb1373828cc266a1eb8a36c0ecef43b587f5e1
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2017-01-28 02:07:12 -0200

    Refactoring

--- index.android.js
+++ index.android.js
@@ -1,10 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+//@flow
 
 import { AppRegistry } from 'react-native';
-import EntriaTest from './app';
+import EntriaTest from './src';
 
 AppRegistry.registerComponent('EntriaTest', () => EntriaTest);
